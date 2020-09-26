074c68bb5219139e40e43b01125ddb9e438e7e27
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2020-09-25 21:06:05 -0300

    add class Matricula

---
+++ src/DigitalHouseManager.kt
@@ -0,0 +1,6 @@
+class DigitalHouseManager(
+        var alunos: MutableList<Aluno>,
+        var professores: MutableList<Professor>,
+        var cursos: MutableList<Curso>,
+        var matriculas: MutableList<Matricula>
+)
