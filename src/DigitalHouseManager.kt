3fbf51b2413c24854e1e32a8225729a14cc6c217
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2020-09-25 22:03:51 -0300

    add funções registrarAluno e excluirAluno na classe DigitalHouseManager

--- src/DigitalHouseManager.kt
+++ src/DigitalHouseManager.kt
@@ -3,4 +3,18 @@ class DigitalHouseManager(
         var professores: MutableList<Professor>,
         var cursos: MutableList<Curso>,
         var matriculas: MutableList<Matricula>
-)
+){
+    fun registrarCurso(nome: String, codigoCurso: Int, quantidadeMaxAlunos: Int) {
+        var novoCurso = Curso(nome = nome, codigo = codigoCurso, quantidadeMaxAlunos = quantidadeMaxAlunos)
+        cursos.add(novoCurso)
+    }
+
+    fun excluirCurso(codigoCurso: Int) {
+        cursos.forEach {
+            if (it.codigo == codigoCurso) {
+                cursos.remove(it)
+            }
+        }
+    }
+    
+}
