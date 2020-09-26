e3b628024ac0475ca67a75db11f0bb0b2228fba6
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2020-09-25 21:14:45 -0300

    add funções adicionarAluno e excluirAluno na classe Curso

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,8 +1,21 @@
-data class Curso (
+class Curso (
         val nome: String,
         val codigo: Int,
         var profTitular: ProfessorTitular,
         var profAdjunto: ProfessorAdjunto,
         val quantidadeMaxAlunos: Int,
         var matriculados: MutableList<Aluno>
-)
+){
+    fun adicionarAluno(aluno: Aluno): Boolean{
+        return if(matriculados.size == quantidadeMaxAlunos){
+            false
+        }else{
+            matriculados.add(aluno)
+            true
+        }
+    }
+
+    fun excluirAluno(aluno: Aluno){
+        matriculados.remove(aluno)
+    }
+}
