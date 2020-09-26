af5a8d61d7339b9b4c9f173467bdb3c35da03401
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2020-09-25 21:00:40 -0300

    add novos atributos na classe Curso

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,4 +1,8 @@
 data class Curso (
         val nome: String,
-        val codigo: Int
+        val codigo: Int,
+        var profTitular: ProfessorTitular,
+        var profAdjunto: ProfessorAdjunto,
+        val quantidadeMaxAlunos: Int,
+        var matriculados: MutableList<Aluno>
 )
